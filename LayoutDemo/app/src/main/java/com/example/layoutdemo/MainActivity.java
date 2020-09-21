fa1684aa6af556c940132ec1869e992bda68369e
Marita Fitzgerald <maritafitzgerald@gmail.com>
2020-09-22 02:29:47 +1000

    initial commit

---
+++ LayoutDemo/app/src/main/java/com/example/layoutdemo/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.layoutdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void SayHi(View view) {
+        Log.d("Here", "here");
+        Toast.makeText(this,"Hi From Test Lab", Toast.LENGTH_LONG).show();
+        int crashy = 1/0;
+    }
+}
